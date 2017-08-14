7ae6abe473764503a79a4a90c4f5b6cfbb6198e1
haibin <zhaohaibin@outlook.com>
2017-08-14 22:52:01 +0800

    modify docker file

--- assets/startup.sh
+++ assets/startup.sh
@@ -4,6 +4,10 @@ set -x
 
 TOMCAT_ADMIN_USER=${TOMCAT_ADMIN_USER:-admin}
 TOMCAT_ADMIN_PASSWORD=${TOMCAT_ADMIN_PASSWORD:-admin}
+TOMCAT_XMX=${XMX:-Xmx128m}
+TOMCAT_XMS=${XMS:-Xms64m}
+TOMCAT_XMN=${XMN:-Xmn32m}
+
 
 
 
@@ -19,6 +23,9 @@ if [ ! -f /opt/tomcat/webapps/isFile ]; then
     mv /opt/tomcat/ROOT.bak /opt/tomcat/webapps/ROOT
 fi
 
-
+if [ echo /opt/tomcat/bin/catalina.sh | grep "{{JAVA_OPTS}}"  ];then
+  TOMCAT_JAVA_OPTS= 'JAVA_OPTS="\"\$JAVA_OPTS\:-Xmx128m -Xms64m -Xmn32m\""'
+  sed 's,'\#\ {{JAVA_OPTS}}','"${TOMCAT_JAVA_OPTS}"',g' -i /opt/tomcat/bin/catalina.sh
+fi
 
 exec supervisord -c /etc/supervisor/supervisord.conf
